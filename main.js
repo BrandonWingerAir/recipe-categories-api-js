1f80b164d273245ebbd6e3e2fabcee41c24a5e59
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-06-16 21:10:48 -0400

    fetch & display api recipes list data

--- main.js
+++ main.js
@@ -24,8 +24,41 @@ let screen = {
 
         if (i == 1) {
             div.classList.add("active");
+            getRecipesOfCategory(categories[i].strCategory);
         }
 
         screen.main.querySelector(".categories").appendChild(div);
     } 
 })();
+
+async function getRecipesOfCategory(category) {
+    screen.main.querySelector(".recipe-list").innerHTML = "";
+
+    try {let res = await fetch(url + "filter.php?c=" + category);
+        let data = await res.json();
+        let recipes = data.meals;
+
+        for (let i = 0; i < recipes.length; i++) {
+            let div = document.createElement("div");
+            div.classList.add("item");
+            div.addEventListener("click", function() {
+                showFullRecipe(recipes[i].idMeal);
+            });
+
+            console.log(recipes);
+
+            div.innerHTML = `
+                <div class="thumbnail">
+                    <img src="${recipes[i].strMealThumb}"/>
+                </div>
+                <div class="details">
+                    <h2>${recipes[i].strMeal}</h2>
+                </div>
+            `;
+
+            screen.main.querySelector(".recipe-list").appendChild(div);
+        }
+    } catch (msg) {
+        
+    }
+}
